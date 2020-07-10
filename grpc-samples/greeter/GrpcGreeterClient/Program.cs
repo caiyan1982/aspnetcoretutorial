00827ececf5f9d4020e3881d7d5c7418c67e2d6b
caiy <caiyan1982@hotmail.com>
2020-07-10 17:32:23 +0800

    grpc start

---
+++ grpc-samples/greeter/GrpcGreeterClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using GrpcGreeter;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var replay = await client.SayHelloAsync(new HelloRequest { Name = "GrpcGreeterClient" });
+            Console.WriteLine("Greeting:" + replay.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
